ab20bdc67de00f98c1e1ce0666404d27c7767bf1
Martim Bello <martimbello@MacBook-Pro-de-Martim.local>
2020-09-22 01:10:57 +0100

    Transfered the project over to react-native-clio

--- App.js
+++ App.js
@@ -1,110 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  TouchableOpacity,
-} from 'react-native';
-import ImagePicker from 'react-native-image-crop-picker';
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import DrawerNavigator from './src/components/screens/DrawerSideBar/DrawerNavigator/DrawerNavigator';
+export default function App() {
+  /*   let [fontsLoaded] = useFonts({
+    "Nunito-Regular": require("./src/assets/fonts/Nunito-Regular.ttf"),
+    "NunitoSans-Regular": require("./src/assets/fonts/NunitoSans-Regular.ttf"),
+    "NunitoSans-Bold": require("./src/assets/fonts/NunitoSans-Bold.ttf"),
+    "Nunito-SemiBold": require("./src/assets/fonts/Nunito-SemiBold.ttf"),
+    "Nunito-Bold": require("./src/assets/fonts/Nunito-Bold.ttf"),
+  });
 
-const App: () => React$Node = () => {
-  const openPicker = () => {
-    ImagePicker.openPicker({
-      width: 500,
-      height: 500,
-      cropping: true,
-      cropperCircleOverlay: true,
-      sortOrder: 'none',
-      compressImageMaxWidth: 1000,
-      compressImageMaxHeight: 1000,
-      compressImageQuality: 1,
-      compressVideoPreset: 'MediumQuality',
-      includeExif: true,
-      cropperStatusBarColor: 'white',
-      cropperToolbarColor: 'white',
-      cropperActiveWidgetColor: 'white',
-      cropperToolbarWidgetColor: '#3498DB',
-    })
-      .then((image) => {
-        console.log('received image', image);
-      })
-      .catch((e) => {
-        console.log(e);
-        Alert.alert(e.message ? e.message : e);
-      });
-  };
-  return (
-    <>
-      <View>
-        <TouchableOpacity
-          style={{
-            height: 50,
-            width: 50,
-            backgroundColor: 'red',
-            alignSelf: 'center',
-            top: 300,
-          }}
-          onPress={() => openPicker()}></TouchableOpacity>
-      </View>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return <DrawerNavigator />;
+  } */
+  return <DrawerNavigator />;
+}
--- src/components/features/PersonalArea/EditCardArea/components/CardForm/CardForm.js
+++ src/components/features/PersonalArea/EditCardArea/components/CardForm/CardForm.js
@@ -1,15 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, TextInput, Dimensions } from "react-native";
 import Styles from "./CardFormStyles";
 import { useForm, Controller } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers";
 import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
+/* import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+import ImageManipulator from "./manipulator/ImageManipulator"; */
+import ImagePicker from "react-native-image-crop-picker";
 import {
   ConnectFacebookIcon,
   ConnectInstagramIcon,
   ConnectLinkedInIcon,
 } from "../../../../../../assets/icons";
+import { Image } from "react-native";
 const projectFormSchema = yup.object().shape({
   name: yup.string().required("*Required"),
   email: yup.string().required("*Required"),
@@ -23,14 +28,69 @@ const CardForm = ({ onClickToSave, redirectSubmittedData }) => {
   const { handleSubmit, trigger, watch, errors, control } = useForm({
     resolver: yupResolver(projectFormSchema),
   });
+  const [image, setImage] = useState(null);
+  const [images, setImages] = useState(null);
   const onSubmit = (data) => {
     redirectSubmittedData(data);
+  };
+  const pickSingle = (cropit, circular = false, mediaType) => {
+    ImagePicker.openPicker({
+      width: 500,
+      height: 500,
+      cropping: cropit,
+      cropperCircleOverlay: circular,
+      sortOrder: "none",
+      compressImageMaxWidth: 1000,
+      compressImageMaxHeight: 1000,
+      compressImageQuality: 1,
+      compressVideoPreset: "MediumQuality",
+      includeExif: true,
+      cropperStatusBarColor: "white",
+      cropperToolbarColor: "white",
+      cropperActiveWidgetColor: "white",
+      cropperToolbarWidgetColor: "#3498DB",
+    })
+      .then((image) => {
+        console.log("received image", image);
+
+        setImage({
+          uri: image.path,
+          width: image.width,
+          height: image.height,
+          mime: image.mime,
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+        Alert.alert(e.message ? e.message : e);
+      });
   };
   useEffect(() => {
     return handleSubmit(onSubmit);
   }, [onClickToSave]);
   return (
     <View style={Styles.outsideContainer}>
+      <View style={{ marginBottom: 15 }}>
+        <Text style={Styles.titleEntries}>Profile Photo</Text>
+
+        <TouchableOpacity
+          style={{ height: 50, width: 50, backgroundColor: "red" }}
+          onPress={() => pickSingle(true, true)}
+        ></TouchableOpacity>
+        {image ? (
+          <Image
+            style={{
+              width: 100,
+              height: 100,
+              resizeMode: "contain",
+              borderRadius: 100 / 2,
+            }}
+            source={image}
+          />
+        ) : (
+          <></>
+        )}
+      </View>
       <View style={{ marginBottom: 15 }}>
         <Text style={Styles.titleEntries}>NAME</Text>
         <Controller
