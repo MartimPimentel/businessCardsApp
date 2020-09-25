258d5d11791c2cd21f5bac507455334023c4bf94
MartimPimentel <martim.b.s.pimentel@gmail.com>
2020-09-25 17:42:44 +0100

    added footer

--- src/components/features/PersonalArea/CardCreatedArea/CardCreatedArea.js
+++ src/components/features/PersonalArea/CardCreatedArea/CardCreatedArea.js
@@ -1,13 +1,14 @@
-import React from "react";
-import { View, Text } from "react-native";
-import Styles from "./CardCreatedAreaStyles";
-import Header from "../../../shared/Header/Header";
-import Card from "../../../shared/Card/Card";
-import { CircleIcon } from "../../../../assets/icons";
+import React from 'react';
+import {View, Text, Dimensions} from 'react-native';
+import Styles from './CardCreatedAreaStyles';
+import Header from '../../../shared/Header/Header';
+import Card from '../../../shared/Card/Card';
+import {QRCodeIcon, NFCIcon} from '../../../../assets/icons';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const CardCreatedArea = () => {
   return (
-    <>
+    <View style={{height: '100%'}}>
       <Header />
       <View style={Styles.textBox}>
         <Text style={Styles.text}>Personal Area</Text>
@@ -15,10 +16,32 @@ const CardCreatedArea = () => {
       <View style={Styles.card}>
         <Card />
       </View>
-      <View style={Styles.card}>
-        <Card />
+      <View style={Styles.divider}>
+        <View style={Styles.outsideContainer}>
+          <View style={Styles.footerBackground}></View>
+          <View style={Styles.buttonsContainer}>
+            <View style={{width: '25%', height: '90%'}}>
+              <TouchableOpacity>
+                <NFCIcon
+                  width="100%"
+                  height="100%"
+                  preserveAspectRatio="meet"
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={{width: '25%', height: '90%'}}>
+              <TouchableOpacity>
+                <QRCodeIcon
+                  width="100%"
+                  height="100%"
+                  preserveAspectRatio="meet"
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
       </View>
-    </>
+    </View>
   );
 };
 export default CardCreatedArea;
--- src/components/screens/PersonalArea/PersonalArea.js
+++ src/components/screens/PersonalArea/PersonalArea.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { View, Text } from "react-native";
-import CardCreatedArea from "../../features/PersonalArea/CardCreatedArea/CardCreatedArea";
-import EditCardArea from "../../features/PersonalArea/EditCardArea/EditCardArea";
-import NoCardArea from "../../features/PersonalArea/NoCardArea/NoCardArea";
+import React from 'react';
+import {View, Text} from 'react-native';
+import CardCreatedArea from '../../features/PersonalArea/CardCreatedArea/CardCreatedArea';
+import EditCardArea from '../../features/PersonalArea/EditCardArea/EditCardArea';
+import NoCardArea from '../../features/PersonalArea/NoCardArea/NoCardArea';
 
 const PersonalArea = () => {
   const personalCardExists = () => {
-    return false;
+    return true;
   };
   if (personalCardExists()) {
     return <CardCreatedArea />;
