781fa10e3f7f8ec055950b48e208aba87be7eeac
MartimPimentel <martim.b.s.pimentel@gmail.com>
2021-02-28 22:49:58 +0000

    go back in scrollView card form

--- src/components/features/MyCardsArea/components/CardForm/CardForm.js
+++ src/components/features/MyCardsArea/components/CardForm/CardForm.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import {View, Text, Dimensions, Linking, Platform} from 'react-native';
+import {
+  View,
+  Text,
+  Dimensions,
+  Linking,
+  Platform,
+  ScrollView,
+} from 'react-native';
 import Styles from './CardFormStyles';
 import {
   FBGoToProfile,
   IGGoToProfile,
   LIGoToProfile,
 } from '../../../../../assets/icons';
-import {
-  TouchableOpacity,
-  ScrollView,
-  TouchableWithoutFeedback,
-} from 'react-native-gesture-handler';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 import {useSelector} from 'react-redux';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -45,173 +48,162 @@ const CardForm = ({onClickForm}) => {
           height: windowHeight <= 600 ? '30%' : '40%',
           width: '100%',
         }}>
-        <TouchableWithoutFeedback onPressIn={() => onClickForm()}>
-          <ScrollView
-            shouldActivateOnStart
-            style={{
-              marginHorizontal: 30,
-            }}
-            keyboardShouldPersistTaps="always">
-            <TouchableOpacity onPressIn={() => onClickForm()}>
-              <View style={Styles.fieldContainer(!!currentCard.name)}>
-                <Text style={Styles.categoryText}>NAME</Text>
-                <View style={Styles.headerContainer}>
-                  <Text style={Styles.informationText}>{currentCard.name}</Text>
-                </View>
-              </View>
-              <View style={Styles.fieldContainer(!!currentCard.companyName)}>
-                <Text style={Styles.categoryText}>COMPANY NAME</Text>
-                <View style={Styles.headerContainer}>
+        <ScrollView
+          shouldActivateOnStart
+          style={{
+            marginHorizontal: 30,
+          }}
+          keyboardShouldPersistTaps="always">
+          <View style={Styles.fieldContainer(!!currentCard.name)}>
+            <Text style={Styles.categoryText}>NAME</Text>
+            <View style={Styles.headerContainer}>
+              <Text style={Styles.informationText}>{currentCard.name}</Text>
+            </View>
+          </View>
+          <View style={Styles.fieldContainer(!!currentCard.companyName)}>
+            <Text style={Styles.categoryText}>COMPANY NAME</Text>
+            <View style={Styles.headerContainer}>
+              <Text style={Styles.informationText}>
+                {currentCard.companyName}
+              </Text>
+            </View>
+          </View>
+          <View style={Styles.fieldContainer(!!currentCard.role)}>
+            <Text style={Styles.categoryText}>COMPANY POSITION</Text>
+            <View style={Styles.headerContainer}>
+              <Text style={Styles.informationText}>{currentCard.role}</Text>
+            </View>
+          </View>
+          <View style={Styles.fieldContainer(!!currentCard.phoneData)}>
+            <Text style={Styles.categoryText}>PHONE NUMBER(PRINCIPAL)</Text>
+            <View style={Styles.headerContainer}>
+              <View style={{width: '55%'}}>
+                <TouchableOpacity
+                  onPress={() =>
+                    openPhoneNumber(
+                      currentCard.phoneData.callingCode,
+                      currentCard.phoneData.phoneNumber,
+                    )
+                  }>
                   <Text style={Styles.informationText}>
-                    {currentCard.companyName}
+                    {!!currentCard.phoneData &&
+                      '+' +
+                        currentCard.phoneData.callingCode +
+                        ' ' +
+                        currentCard.phoneData.phoneNumber}
                   </Text>
-                </View>
+                </TouchableOpacity>
               </View>
-              <View style={Styles.fieldContainer(!!currentCard.role)}>
-                <Text style={Styles.categoryText}>COMPANY POSITION</Text>
-                <View style={Styles.headerContainer}>
-                  <Text style={Styles.informationText}>{currentCard.role}</Text>
-                </View>
+            </View>
+          </View>
+          <View
+            style={Styles.fieldContainer(!!currentCard.alternativePhoneData)}>
+            <Text style={Styles.categoryText}>PHONE NUMBER(ALTERNATIVE)</Text>
+            <View style={Styles.headerContainer}>
+              <View style={{width: '50%'}}>
+                <TouchableOpacity>
+                  <Text
+                    onPress={() =>
+                      openPhoneNumber(
+                        currentCard.alternativePhoneData.callingCode,
+                        currentCard.alternativePhoneData.phoneNumber,
+                      )
+                    }
+                    style={Styles.informationText}>
+                    {!!currentCard.alternativePhoneData &&
+                      '+' +
+                        currentCard.alternativePhoneData.callingCode +
+                        ' ' +
+                        currentCard.alternativePhoneData.phoneNumber}
+                  </Text>
+                </TouchableOpacity>
               </View>
-              <View style={Styles.fieldContainer(!!currentCard.phoneData)}>
-                <Text style={Styles.categoryText}>PHONE NUMBER(PRINCIPAL)</Text>
-                <View style={Styles.headerContainer}>
-                  <View style={{width: '55%'}}>
-                    <TouchableOpacity>
-                      <Text
-                        onPress={() =>
-                          openPhoneNumber(
-                            currentCard.phoneData.callingCode,
-                            currentCard.phoneData.phoneNumber,
-                          )
-                        }
-                        style={Styles.informationText}>
-                        {!!currentCard.phoneData &&
-                          '+' +
-                            currentCard.phoneData.callingCode +
-                            ' ' +
-                            currentCard.phoneData.phoneNumber}
-                      </Text>
-                    </TouchableOpacity>
-                  </View>
-                </View>
-              </View>
-              <View
-                style={Styles.fieldContainer(
-                  !!currentCard.alternativePhoneData,
-                )}>
-                <Text style={Styles.categoryText}>
-                  PHONE NUMBER(ALTERNATIVE)
+            </View>
+          </View>
+          <View style={Styles.fieldContainer(!!currentCard.email)}>
+            <Text style={Styles.categoryText}>EMAIL</Text>
+            <View style={Styles.headerContainer}>
+              <TouchableOpacity>
+                <Text
+                  onPress={() => {
+                    openLink('mailto:' + currentCard.email);
+                  }}
+                  style={Styles.informationText}>
+                  {currentCard.email}
                 </Text>
-                <View style={Styles.headerContainer}>
-                  <View style={{width: '50%'}}>
-                    <TouchableOpacity>
-                      <Text
-                        onPress={() =>
-                          openPhoneNumber(
-                            currentCard.alternativePhoneData.callingCode,
-                            currentCard.alternativePhoneData.phoneNumber,
-                          )
-                        }
-                        style={Styles.informationText}>
-                        {!!currentCard.alternativePhoneData &&
-                          '+' +
-                            currentCard.alternativePhoneData.callingCode +
-                            ' ' +
-                            currentCard.alternativePhoneData.phoneNumber}
-                      </Text>
-                    </TouchableOpacity>
-                  </View>
-                </View>
-              </View>
-              <View style={Styles.fieldContainer(!!currentCard.email)}>
-                <Text style={Styles.categoryText}>EMAIL</Text>
-                <View style={Styles.headerContainer}>
-                  <TouchableOpacity>
-                    <Text
-                      onPress={() => {
-                        openLink('mailto:' + currentCard.email);
-                      }}
-                      style={Styles.informationText}>
-                      {currentCard.email}
-                    </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={Styles.fieldContainer(!!currentCard.address)}>
+            <Text style={Styles.categoryText}>ADDRESS</Text>
+            <View style={Styles.headerContainer}>
+              <Text style={Styles.informationText}>{currentCard.address}</Text>
+            </View>
+          </View>
+          <View style={Styles.fieldContainer(!!currentCard.observations)}>
+            <Text style={Styles.categoryText}>OBSERVATIONS</Text>
+            <View style={Styles.headerContainer}>
+              <Text style={Styles.informationText}>
+                {currentCard.observations}
+              </Text>
+            </View>
+          </View>
+          {(!!currentCard.facebookLink ||
+            !!currentCard.instagramLink ||
+            !!currentCard.linkedInLink) && (
+            <View>
+              <Text style={Styles.categoryText}>SOCIAL NETWORKS</Text>
+              <View style={Styles.linksContainer}>
+                <View style={{height: '10%'}}></View>
+                {!!currentCard.facebookLink && (
+                  <TouchableOpacity
+                    style={Styles.iconsTouchableContainer}
+                    onPress={() => {
+                      Linking.openURL(currentCard.facebookLink);
+                    }}>
+                    <View style={Styles.iconsContainer}>
+                      <FBGoToProfile
+                        viewBox={windowWidth <= 350 ? '10 -5 250 55' : ''}
+                      />
+                    </View>
                   </TouchableOpacity>
-                </View>
-              </View>
-              <View style={Styles.fieldContainer(!!currentCard.address)}>
-                <Text style={Styles.categoryText}>ADDRESS</Text>
-                <View style={Styles.headerContainer}>
-                  <Text style={Styles.informationText}>
-                    {currentCard.address}
-                  </Text>
-                </View>
-              </View>
-              <View style={Styles.fieldContainer(!!currentCard.observations)}>
-                <Text style={Styles.categoryText}>OBSERVATIONS</Text>
-                <View style={Styles.headerContainer}>
-                  <Text style={Styles.informationText}>
-                    {currentCard.observations}
-                  </Text>
-                </View>
-              </View>
-              {(!!currentCard.facebookLink ||
-                !!currentCard.instagramLink ||
-                !!currentCard.linkedInLink) && (
-                <View>
-                  <Text style={Styles.categoryText}>SOCIAL NETWORKS</Text>
-                  <View style={Styles.linksContainer}>
-                    <View style={{height: '10%'}}></View>
-                    {!!currentCard.facebookLink && (
-                      <TouchableOpacity
-                        style={Styles.iconsTouchableContainer}
-                        onPress={() => {
-                          Linking.openURL(currentCard.facebookLink);
-                        }}>
-                        <View style={Styles.iconsContainer}>
-                          <FBGoToProfile
-                            viewBox={windowWidth <= 350 ? '10 -5 250 55' : ''}
-                          />
-                        </View>
-                      </TouchableOpacity>
-                    )}
+                )}
 
-                    {!!currentCard.instagramLink && (
-                      <>
-                        <View style={{height: '8%'}}></View>
-                        <TouchableOpacity
-                          style={Styles.iconsTouchableContainer}
-                          onPress={() => {
-                            Linking.openURL(currentCard.instagramLink);
-                          }}>
-                          <View style={Styles.iconsContainer}>
-                            <IGGoToProfile
-                              viewBox={windowWidth <= 350 ? '10 -5 250 55' : ''}
-                            />
-                          </View>
-                        </TouchableOpacity>
-                      </>
-                    )}
+                {!!currentCard.instagramLink && (
+                  <>
                     <View style={{height: '8%'}}></View>
-                    {!!currentCard.linkedInLink && (
-                      <TouchableOpacity
-                        style={Styles.iconsTouchableContainer}
-                        onPress={() => {
-                          Linking.openURL(currentCard.linkedInLink);
-                        }}>
-                        <View style={Styles.iconsContainer}>
-                          <LIGoToProfile
-                            viewBox={windowWidth <= 350 ? '10 -5 250 55' : ''}
-                          />
-                        </View>
-                      </TouchableOpacity>
-                    )}
-                    <View style={{height: '8%'}}></View>
-                  </View>
-                </View>
-              )}
-            </TouchableOpacity>
-          </ScrollView>
-        </TouchableWithoutFeedback>
+                    <TouchableOpacity
+                      style={Styles.iconsTouchableContainer}
+                      onPress={() => {
+                        Linking.openURL(currentCard.instagramLink);
+                      }}>
+                      <View style={Styles.iconsContainer}>
+                        <IGGoToProfile
+                          viewBox={windowWidth <= 350 ? '10 -5 250 55' : ''}
+                        />
+                      </View>
+                    </TouchableOpacity>
+                  </>
+                )}
+                <View style={{height: '8%'}}></View>
+                {!!currentCard.linkedInLink && (
+                  <TouchableOpacity
+                    style={Styles.iconsTouchableContainer}
+                    onPress={() => {
+                      Linking.openURL(currentCard.linkedInLink);
+                    }}>
+                    <View style={Styles.iconsContainer}>
+                      <LIGoToProfile
+                        viewBox={windowWidth <= 350 ? '10 -5 250 55' : ''}
+                      />
+                    </View>
+                  </TouchableOpacity>
+                )}
+                <View style={{height: '8%'}}></View>
+              </View>
+            </View>
+          )}
+        </ScrollView>
       </View>
     )
   );
