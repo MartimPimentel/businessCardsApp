01478f88fe9a42d234cb411d37f8977be2e08f57
MartimPimentel <martim.b.s.pimentel@gmail.com>
2021-03-05 00:17:22 +0000

    .

--- src/components/screens/auth/PersonalArea/PersonalArea.js
+++ src/components/screens/auth/PersonalArea/PersonalArea.js
@@ -1,21 +1,16 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import CardCreatedArea from '../../../features/PersonalArea/CardCreatedArea/CardCreatedArea';
 import NoCardArea from '../../../features/PersonalArea/NoCardArea/NoCardArea';
 import {createStackNavigator} from '@react-navigation/stack';
 import EditCardArea from '../../../features/PersonalArea/EditCardArea/EditCardArea';
 import {usePersonalCard} from '../../../../shared/api/usePersonalCard';
-import {AsyncContext} from '../../../../shared/providers/AsyncProvider';
 
 const Stack = createStackNavigator();
 const PersonalArea = () => {
   const {card, getPersonalCard} = usePersonalCard();
-  const [ready, setReady] = useState(false);
-  const {error, loading, setError, setLoading} = useContext(AsyncContext);
   useEffect(() => {
-    setError({initError: 'error'});
     getPersonalCard();
   }, []);
-  console.log(card);
   return card ? (
     <Stack.Navigator
       headerMode="none"
